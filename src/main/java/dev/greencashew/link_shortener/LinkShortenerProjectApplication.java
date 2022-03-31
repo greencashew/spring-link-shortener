787a46d655842df61f7aa8c7187039f41c1b9a6e
greencashew <greencashew@users.noreply.github.com>
2022-04-01 00:43:19 +0200

    Init Spring Boot Application

---
+++ src/main/java/dev/greencashew/link_shortener/LinkShortenerProjectApplication.java
@@ -0,0 +1,12 @@
+package dev.greencashew.link_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/greencashew/link_shortener/LinkShortenerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.greencashew.link_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
