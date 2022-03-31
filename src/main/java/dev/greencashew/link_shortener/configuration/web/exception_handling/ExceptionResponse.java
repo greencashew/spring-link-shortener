fb36a27904154f87e907cf53240bc08bb70a8915
greencashew <greencashew@users.noreply.github.com>
2022-04-01 00:44:55 +0200

    Add global rest controller exception handler

---
+++ src/main/java/dev/greencashew/link_shortener/configuration/web/exception_handling/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package dev.greencashew.link_shortener.configuration.web.exception_handling;
+
+public record ExceptionResponse(String errorMessage) {
+}
---
+++ src/main/java/dev/greencashew/link_shortener/configuration/web/exception_handling/GlobalControllerAdvisor.java
@@ -0,0 +1,20 @@
+package dev.greencashew.link_shortener.configuration.web.exception_handling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+@Slf4j
+class GlobalControllerAdvisor {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ExceptionResponse handleError(Exception ex, WebRequest request) {
+        log.error("Unexpected exception for web request: " + request, ex);
+        return new ExceptionResponse(ex.getMessage());
+    }
+}
